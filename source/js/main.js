3f95c47053b9b987ea388ae0608523a5c12125bc
ws-morrison <workshop.morrison@gmail.com>
2018-09-07 21:22:49 +0300

    add owlCarousel

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,23 @@
 "use strict";
 
-// $(".burger").click(function() {
-//   $(".header-nav__list").toggleClass("visually-hidden");
-// });
- console.log('Привет');
+// OwlCarousel
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel();
+});
+
+$(".loop").owlCarousel({
+  center: true,
+  items: 1,
+  loop: true,
+  margin: 10,
+  responsive: {
+    600: {
+      items: 1
+    }
+  },
+  singleItem: true,
+  autoHeight: false,
+  nav: true,
+  navText: ['<img src=\"/source/img/slider-prev.png\">','<img src=\"/source/img/slider-prev.png\">']
+});
+
