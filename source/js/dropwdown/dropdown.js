c2e6605a380f9ab977abff9af894c26edade1e85
Alexey Makarov <alexeimakarow@gmail.com>
2018-09-10 15:58:20 +0300

    asdasdaasd

--- source/js/dropwdown/dropdown.js
+++ source/js/dropwdown/dropdown.js
@@ -1,30 +1,33 @@
 "use strict";
 
-$(document).ready(function() {
-  // var ActivateList = function() {
+$(document).ready(function () {
+    // var ActivateList = function() {
 
-  //   $(".dropdown-list__heading").toggleClass("dropdown-list__heading--active");
-  //   $(".docs__wrapper")
-  //     .toggleClass(".visually-hidden")
-  //     .fadeToggle("fast");
+    //   $(".dropdown-list__heading").toggleClass("dropdown-list__heading--active");
+    //   $(".docs__wrapper")
+    //     .toggleClass(".visually-hidden")
+    //     .fadeToggle("fast");
 
-  // };
+    // };
 
-  // $(".dropdown-list__heading").click(function() {
-  //   $(".dropdown-list__heading").toggleClass("dropdown-list__heading--active");
-  //   $(".docs__wrapper")
-  //     .toggleClass(".visually-hidden")
-  //     .fadeToggle("fast");
-  // });
+    // $(".dropdown-list__heading").click(function() {
+    //   $(".dropdown-list__heading").toggleClass("dropdown-list__heading--active");
+    //   $(".docs__wrapper")
+    //     .toggleClass(".visually-hidden")
+    //     .fadeToggle("fast");
+    // });
 
-  $(".dropdown-list__heading--active").each(function() {
-    $(this).click(function() {
-        $(this).toggleClass("dropdown-list__heading--active");
-        $(this).closest("docs__wrapper")
-          .toggleClass("visually-hidden")
-          .fadeToggle("fast");
+    $(".docs__wrapper").addClass("visually-hidden");
+
+    $(".dropdown-list__item").each(function () {
+        $(this).on('click', function () {
+
+            $(this).children('.dropdown-list__heading').toggleClass("dropdown-list__heading--active");
+
+            $(this).children('.docs__wrapper').toggleClass("visually-hidden");
+
+        });
     });
-  });
 });
 
 // Алексей Макаров, [10.09.18 15: 33]
