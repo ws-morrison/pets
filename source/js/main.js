6e91b6663b04e2965b7a37f54b5328daf7e1fda2
ws-morrison <workshop.morrison@gmail.com>
2018-09-12 14:09:31 +0300

    add nav menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,16 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
     // change nav-button background
-    $(document).on('click', '.nav-list__button', function (evt) {
-        console.log('asd');
+    $(document).on("click", ".nav-list__button", function(evt) {
         evt.preventDefault();
-        $('.nav-list__button').removeClass('nav-list__button--active');
-        $(this).addClass('nav-list__button--active');
-    })
+        $(".nav-list__button").removeClass("nav-list__button--active");
+        $(this).addClass("nav-list__button--active");
+    });
+
+    // burger
+    // $(".nav-menu").addClass("visually-hidden");
+
+    $(".burger").on("click", function() {
+        console.log("asd");
+        $(".nav-menu").toggleClass("visually-hidden").fadeTo('slow');
+    });
 });
