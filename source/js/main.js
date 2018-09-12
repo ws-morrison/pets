8f92f15007ccb30e3b5b84b273959788f12aea44
ws-morrison <workshop.morrison@gmail.com>
2018-09-12 19:12:36 +0300

    burger, animation, fxs

--- source/js/main.js
+++ source/js/main.js
@@ -6,11 +6,28 @@ $(document).ready(function() {
         $(this).addClass("nav-list__button--active");
     });
 
+
+    // change nav-button orange background
+    $(document).on("click", ".nav-list__button", function (evt) {
+        evt.preventDefault();
+        $(".nav-list__button").removeClass("nav-list__button--orange");
+        $(this).addClass("nav-list__button--orange");
+    });
     // burger
     // $(".nav-menu").addClass("visually-hidden");
 
-    $(".burger").on("click", function() {
-        console.log("asd");
+    // special for index
+    $(".burger--special").on("click", function() {
+        $(".nav-menu--special").toggleClass("visually-hidden").fadeTo('slow');
+    });
+
+    // open/close menu
+    $(".burger").on("click", function () {
         $(".nav-menu").toggleClass("visually-hidden").fadeTo('slow');
+    });
+
+    // Burger animation
+    $('.burger').click(function () {
+        $(this).toggleClass('open');
     });
 });
