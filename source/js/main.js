b50d201dc8dcfba9f34c9f2815dd2c591602fd0c
ws-morrison <workshop.morrison@gmail.com>
2018-09-13 12:43:55 +0300

    burger, grid fxs

--- source/js/main.js
+++ source/js/main.js
@@ -6,28 +6,35 @@ $(document).ready(function() {
         $(this).addClass("nav-list__button--active");
     });
 
-
     // change nav-button orange background
-    $(document).on("click", ".nav-list__button--catalog", function (evt) {
+    $(document).on("click", ".nav-list__button--catalog", function(evt) {
         evt.preventDefault();
         $(".nav-list__button--catalog").removeClass("nav-list__button--orange");
         $(this).addClass("nav-list__button--orange");
     });
+
     // burger
-    // $(".nav-menu").addClass("visually-hidden");
+    // open/close menu
+    $(".burger").on("click", function() {
+        console.log("Нажал на бургер");
+        $(".nav-menu").toggleClass("visually-hidden");
+    });
 
     // special for index
     $(".burger--special").on("click", function() {
-        $(".nav-menu--special").toggleClass("visually-hidden").fadeTo('slow');
-    });
-
-    // open/close menu
-    $(".burger").on("click", function () {
-        $(".nav-menu").toggleClass("visually-hidden").fadeTo('slow');
+        $(".nav-menu--special").toggleClass("visually-hidden");
     });
 
     // Burger animation
-    $('.burger').click(function () {
-        $(this).toggleClass('open');
+    $(".burger").click(function() {
+        $(this).toggleClass("open");
+    });
+
+    // stopPropagation Burger menu
+    $(document).click(function(event) {
+        if ($(event.target).closest(".burger").length) return;
+        $(".burger").removeClass("open");
+        $(".nav-menu").addClass("visually-hidden");
+        event.stopPropagation();
     });
 });
