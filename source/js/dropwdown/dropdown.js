6fd7122cfc67a6b1364ceefc80b0bf9de4afae77
ws-morrison <workshop.morrison@gmail.com>
2018-10-03 15:52:19 +0300

    //

--- source/js/dropwdown/dropdown.js
+++ source/js/dropwdown/dropdown.js
@@ -1,16 +1,19 @@
 "use strict";
 
-$(document).ready(function () {
-
+$(document).ready(function() {
     $(".docs__wrapper").addClass("visually-hidden");
 
-    $(".dropdown-list__item").each(function () {
-        $(this).on('click', function () {
-
-
-            $(this).children('.dropdown-list__heading').toggleClass("dropdown-list__heading--active");
-            $(this).find('.dropdown-list__heading-thumb').toggleClass("dropdown-list__heading-thumb--active");
-            $(this).children('.docs__wrapper').toggleClass("visually-hidden");
+    $(".dropdown-list__item").each(function() {
+        $(this).on("click", function() {
+            $(this)
+                .children(".dropdown-list__heading")
+                .toggleClass("dropdown-list__heading--active");
+            $(this)
+                .find(".dropdown-list__heading-thumb")
+                .toggleClass("dropdown-list__heading-thumb--active");
+            $(this)
+                .children(".docs__wrapper")
+                .toggleClass("visually-hidden");
         });
     });
 });
