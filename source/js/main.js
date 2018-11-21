cdc3485296042f5b614a084bb0c08d9831f0db40
ws-morrison <workshop.morrison@gmail.com>
2018-11-21 18:38:42 +0300

    add blocks, pages

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // change nav-button background
-    $(document).on("click", ".nav-list__button", function(evt) {
+    $(document).on("click", ".nav-list__button", function (evt) {
         evt.preventDefault();
         $(".nav-list__button").removeClass("nav-list__button--active");
         $(this).addClass("nav-list__button--active");
     });
 
     // change nav-button orange background
-    $(document).on("click", ".nav-list__button--catalog", function(evt) {
+    $(document).on("click", ".nav-list__button--catalog", function (evt) {
         evt.preventDefault();
         $(".nav-list__button--catalog").removeClass("nav-list__button--orange");
         $(this).addClass("nav-list__button--orange");
@@ -15,25 +15,25 @@ $(document).ready(function() {
 
     // burger
     // open/close menu
-    $(".burger").on("click", function() {
+    $(".burger").on("click", function () {
         $(".nav-menu").toggleClass("visually-hidden");
         $(".modal-overlay").removeClass("hidden");
     });
 
     // special for index
-    $(".burger--special").on("click", function() {
+    $(".burger--special").on("click", function () {
         $(".menu-nav").toggleClass("visually-hidden");
     });
 
     // Burger animation
-    $(".burger").click(function() {
+    $(".burger").click(function () {
         $(this).toggleClass("open");
         $(".nav-menu").toggleClass("nav-menu--opened");
         $(".menu-nav").toggleClass("nav-menu--opened");
     });
 
     // stopPropagation Burger menu
-    $(".modal-overlay").click(function(event) {
+    $(".modal-overlay").click(function (event) {
         if ($(event.target).closest(".burger").length) return;
         $(".burger").removeClass("open");
         $(".nav-menu").addClass("visually-hidden");
@@ -42,5 +42,32 @@ $(document).ready(function() {
         $(".menu-nav").addClass("visually-hidden");
         $(".modal-overlay").toggleClass("hidden");
         event.stopPropagation();
+    });
+
+
+    // Показ модального при покидании сайта
+    var exitModal = $(".modal");
+    document.addEventListener("mouseleave", function (e) {
+        if (e.clientY < 0) {
+            $("html, body").animate({
+                    scrollTop: exitModal.offset().top
+                },
+                500
+            );
+            exitModal.show(100);
+            // exitModal.toggleClass('show');
+        }
+    });
+
+    // Закрываем модальное окно
+    function modalClose() {
+        $(".modal").removeClass("show");
+        $(".modal-overlay").addClass("visually-hidden");
+    }
+
+    $(".jsModalCloseBtn").on("click", function (e) {
+        e.preventDefault();
+        exitModal.hide(100);
+        // modalClose();
     });
 });
