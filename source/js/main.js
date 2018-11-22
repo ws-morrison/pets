932d0ed7595e9d9c93a5449fe06138f4ca5d3a13
ws-morrison <workshop.morrison@gmail.com>
2018-11-22 21:15:36 +0300

    add pages, blocks, js

--- source/js/main.js
+++ source/js/main.js
@@ -1,55 +1,55 @@
 $(document).ready(function () {
     // change nav-button background
-    $(document).on("click", ".nav-list__button", function (evt) {
+    $(document).on('click', '.nav-list__button', function (evt) {
         evt.preventDefault();
-        $(".nav-list__button").removeClass("nav-list__button--active");
-        $(this).addClass("nav-list__button--active");
+        $('.nav-list__button').removeClass('nav-list__button--active');
+        $(this).addClass('nav-list__button--active');
     });
 
     // change nav-button orange background
-    $(document).on("click", ".nav-list__button--catalog", function (evt) {
+    $(document).on('click', '.nav-list__button--catalog', function (evt) {
         evt.preventDefault();
-        $(".nav-list__button--catalog").removeClass("nav-list__button--orange");
-        $(this).addClass("nav-list__button--orange");
+        $('.nav-list__button--catalog').removeClass('nav-list__button--orange');
+        $(this).addClass('nav-list__button--orange');
     });
 
     // burger
     // open/close menu
-    $(".burger").on("click", function () {
-        $(".nav-menu").toggleClass("visually-hidden");
-        $(".modal-overlay").removeClass("hidden");
+    $('.burger').on('click', function () {
+        $('.nav-menu').toggleClass('visually-hidden');
+        $('.modal-overlay').removeClass('hidden');
     });
 
     // special for index
-    $(".burger--special").on("click", function () {
-        $(".menu-nav").toggleClass("visually-hidden");
+    $('.burger--special').on('click', function () {
+        $('.menu-nav').toggleClass('visually-hidden');
     });
 
     // Burger animation
-    $(".burger").click(function () {
-        $(this).toggleClass("open");
-        $(".nav-menu").toggleClass("nav-menu--opened");
-        $(".menu-nav").toggleClass("nav-menu--opened");
+    $('.burger').click(function () {
+        $(this).toggleClass('open');
+        $('.nav-menu').toggleClass('nav-menu--opened');
+        $('.menu-nav').toggleClass('nav-menu--opened');
     });
 
     // stopPropagation Burger menu
-    $(".modal-overlay").click(function (event) {
-        if ($(event.target).closest(".burger").length) return;
-        $(".burger").removeClass("open");
-        $(".nav-menu").addClass("visually-hidden");
-        $(".nav-menu").removeClass("nav-menu--opened");
-        $(".menu-nav").removeClass("nav-menu--opened");
-        $(".menu-nav").addClass("visually-hidden");
-        $(".modal-overlay").toggleClass("hidden");
+    $('.modal-overlay').click(function (event) {
+        if ($(event.target).closest('.burger').length) return;
+        $('.burger').removeClass('open');
+        $('.nav-menu').addClass('visually-hidden');
+        $('.nav-menu').removeClass('nav-menu--opened');
+        $('.menu-nav').removeClass('nav-menu--opened');
+        $('.menu-nav').addClass('visually-hidden');
+        $('.modal-overlay').toggleClass('hidden');
         event.stopPropagation();
     });
 
 
     // Показ модального при покидании сайта
-    var exitModal = $(".modal");
-    document.addEventListener("mouseleave", function (e) {
+    var exitModal = $('.modal');
+    document.addEventListener('mouseleave', function (e) {
         if (e.clientY < 0) {
-            $("html, body").animate({
+            $('html, body').animate({
                     scrollTop: exitModal.offset().top
                 },
                 500
@@ -60,14 +60,31 @@ $(document).ready(function () {
     });
 
     // Закрываем модальное окно
-    function modalClose() {
-        $(".modal").removeClass("show");
-        $(".modal-overlay").addClass("visually-hidden");
-    }
+    // function modalClose() {
+    //     $('.modal').removeClass('show');
+    //     $('.modal-overlay').addClass('visually-hidden');
+    // }
 
-    $(".jsModalCloseBtn").on("click", function (e) {
+    $('.jsModalCloseBtn').on('click', function (e) {
         e.preventDefault();
         exitModal.hide(100);
         // modalClose();
     });
+
+
+    // Drop-items
+    var showContentTrigger = $('.jsShowContentTrigger');
+    var showContent = $('.jsShowContent');
+
+    // showContent.addClass('hidden');
+    showContentTrigger.each(function () {
+        $(this).on('click', function () {
+        $(this)
+            .find(".icon__plus-drop")
+            .toggleClass("active");
+        $(this).toggleClass('active');
+        $(this).next(showContent).toggleClass('active');
+        })
+    });
+
 });
